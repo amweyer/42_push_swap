7f22f80618a8b2f2f7221f37cb2ee2fc512396e6
Amaya <amaya.weyer@gmail.com>
2025-05-29 01:16:44 +0200

    tsdcsq

--- errors.c
+++ errors.c
@@ -1,24 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amayaweyer <amayaweyer@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/27 11:54:06 by amayaweyer        #+#    #+#             */
-/*   Updated: 2025/05/27 12:18:55 by amayaweyer       ###   ########.fr       */
+/*   Created: 2025/05/27 12:59:14 by amayaweyer        #+#    #+#             */
+/*   Updated: 2025/05/28 15:10:02 by amayaweyer       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int ac, char **av)
+int ft_check_synthax(char *arg)
 {
-    char **argv;
     
-    if(ac == 2)
-        argv = ft_split(av, ' ');
-    init_stack(argv);        
-        
-    return(1);
+}
+
+int ft_check_duplicates(t_stack **a, int nb)
+{
+    
+}
+
+int ft_check_errors(char *arg)
+{
+    if(ft_check_duplicates(arg))
+        return(1);
+    if(ft_check_synthax(arg))
+        return(1);
+    
+    
 }
--- stack_init.c
+++ stack_init.c
@@ -6,7 +6,26 @@
 /*   By: amayaweyer <amayaweyer@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 11:55:20 by amayaweyer        #+#    #+#             */
-/*   Updated: 2025/05/27 11:55:21 by amayaweyer       ###   ########.fr       */
+/*   Updated: 2025/05/28 15:06:21 by amayaweyer       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+
+//ft_atol 
+
+void stack_creator(t_stack **a, char **av)
+{
+    int i;
+
+    i = 0;
+    while(av[i])
+    {
+        if(ft_check_errors(av[i]))
+        {
+            //free(stask)
+            return(1);
+        }
+    }
+}
